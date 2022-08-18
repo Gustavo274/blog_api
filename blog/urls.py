cf3794434c3a2384c57187b6171f9bed59211ee6
Gustavo <gustavocota98@gmail.com>
2022-08-18 01:07:13 -0300

    JWT Authentication integration through simplejwt

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from articles import views
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -14,6 +17,8 @@ urlpatterns = [
 
     # Autenticação da api
     path("user/", include("users.urls", namespace="users")),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Api
     path("articles/", include("articles.urls", namespace="articles_api")),
