999e4498e26a92132071dcae49c06948c00779d2
Gustavo <gustavocota98@gmail.com>
2022-08-18 01:02:03 -0300

    Creation of the blog app, base of the project

---
+++ blog/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from articles import views
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    # home
+    path('', views.ApiOverview, name='home'),
+
+    # Login pelo browser
+    path("api-auth/", include("rest_framework.urls")),
+
+    # Autenticação da api
+    path("user/", include("users.urls", namespace="users")),
+
+    # Api
+    path("articles/", include("articles.urls", namespace="articles_api")),
+]
